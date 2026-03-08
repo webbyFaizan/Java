af46a4ac146ccd201f506f09903ca7b1463eedbd
Faizan Ansari <faizanansari989293@gmail.com>
2026-03-08 16:54:15 +0530

    update

---
+++ Array/Practice/PatternPractice.java
@@ -0,0 +1,66 @@
+package Array.Practice;
+
+public class PatternPractice {
+    public static void main(String[] args) {
+        // int n = 5;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n = 5;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n = 5;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n = 4;
+        // int num = 1;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(num);
+        //         num++;
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n = 5;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n = 5;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=1;k<=i;k++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        int n = 5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

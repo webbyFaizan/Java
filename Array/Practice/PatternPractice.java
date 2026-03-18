3eb44cc61813fb4ead8b1a687758a0107d4cbea4
Faizan Ansari <faizanansari989293@gmail.com>
2026-03-18 16:24:53 +0530

    update

--- Array/Practice/PatternPractice.java
+++ Array/Practice/PatternPractice.java
@@ -100,25 +100,61 @@ public class PatternPractice {
         //     System.out.println();
         // }
 
-        int n = 5;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
-            for(int k=1;k<=2*i-1;k++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        // int n = 5;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=1;k<=2*i-1;k++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
 
-        for(int i=n-1;i>=1;i--){
-            for(int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
-            for(int k=1;k<=2*i-1;k++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        // for(int i=n-1;i>=1;i--){
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=1;k<=2*i-1;k++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n = 5;
+        // for(int i=1;i<=n;i++){
+        //     int num = i % 2;
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(num);
+        //         num = 1 - num;
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n = 5;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for( int j=1;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     for(int j=i-1;j>=1;j--){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n = 4;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         if(i == 1 || i== n || j == 1 || j == n){
+        //             System.out.print("*");
+        //         }else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
     }
 }
